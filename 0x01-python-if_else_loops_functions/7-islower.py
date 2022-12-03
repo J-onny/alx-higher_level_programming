b596eac30cb575936004ec9612b257be9cea944c
j-onny <omo.pro22@gmail.com>
2022-12-02 18:58:53 -0800

    if_else_loops1.0

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+    for i in (range(ord('a') and ord('Z'))):
+        if ord(c) > 96 and ord(c) < 123:
+            return True
+        else:
+            return False
+
