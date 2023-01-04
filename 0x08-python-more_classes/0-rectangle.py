0050b348643aae4f365d6896b1be417e2ee0a8e7
j-onny <omo.pro22@gmail.com>
2023-01-05 01:27:56 +0300

    objects&classess1

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+        Attributes:
+        empty
+    """
+
+    pass
